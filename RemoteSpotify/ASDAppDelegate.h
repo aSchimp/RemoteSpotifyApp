d86acb50fa3302c27e45694231f9da236d4981fd
Alex Schimp <alex@Alexs-Mac-Pro.local>
2014-02-03 22:48:59 -0600

    Initial Commit

---
+++ RemoteSpotify/ASDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ASDAppDelegate.h
+//  RemoteSpotify
+//
+//  Created by Alex Schimp on 2/3/14.
+//  Copyright (c) 2014 Alex Schimp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
